0dcd158d7cb185ff89fcd3c126a95c8689cd18fd
sreebash <sreebashdas.1@gmail.com>
2022-07-20 07:49:53 +0600

    retrive signle NoteItem

--- src/pages/NotePage.js
+++ src/pages/NotePage.js
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {useParams} from "react-router-dom";
+import notesListPage from "./NotesListPage";
 
 const NotePage = () => {
     const {noteId} = useParams();
-    console.log('noteId:', noteId)
+    
+    let [note, setNote] = useState({});
+    
+    const getNote = async () => {
+        let response = await fetch(`/api/notes/${noteId}`)
+        let data = await response.json()
+        setNote(data)
+    }
+    
+    useEffect(() => {
+        getNote()
+    }, [noteId])
     return (
         <div>
-            <h1>Single Note {noteId}</h1>
+            <p>{note.body}</p>
         
         </div>
     );
